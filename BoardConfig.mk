ff1992365f02b0d6d0adf96fa9fc7f8151b1be5a
pavanpaps <pavanishot200@gmail.com>
2021-05-26 19:07:20 +0200

    raphael: BoardConfig: Restore board sepolicy flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,8 +185,7 @@ USE_SENSOR_MULTI_HAL := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
-BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_raphael
